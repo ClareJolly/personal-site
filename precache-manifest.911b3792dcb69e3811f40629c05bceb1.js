ac99ec9001dd92a12e480bc7c2d8d14a85be3185
Clare Jolly <clajollyuk@gmail.com>
2019-01-02 14:09:20 +0000

    Updates

---
+++ precache-manifest.911b3792dcb69e3811f40629c05bceb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ebdd6b1870a2709c31",
+    "url": "/personal-site/static/js/runtime~main.60ebdd6b.js"
+  },
+  {
+    "revision": "9cac236289ee6a880c9b",
+    "url": "/personal-site/static/js/main.9cac2362.chunk.js"
+  },
+  {
+    "revision": "f91658b14890458fd99f",
+    "url": "/personal-site/static/js/1.f91658b1.chunk.js"
+  },
+  {
+    "revision": "9cac236289ee6a880c9b",
+    "url": "/personal-site/static/css/main.c47ea6cb.chunk.css"
+  },
+  {
+    "revision": "85f49ff35b5940a56cfab9014d936fd8",
+    "url": "/personal-site/index.html"
+  }
+];
